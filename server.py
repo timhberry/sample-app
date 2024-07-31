1c6e84f0830fc33b44427b7e858ee3cf7064bdb9
Tim Berry <timhberry@gmail.com>
2024-07-31 17:15:16 +0100

    Change to green

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import tornado.web
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         title = "Hello, World!"
-        bgcolor = "dodgerblue"
+        bgcolor = "olivedrab"
         self.render("template.html", title=title, bgcolor=bgcolor)
         print(self.request)
 
